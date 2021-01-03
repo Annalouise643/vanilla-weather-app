bb136ca11105e4b14e31e26f58c661ef47badc84
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2021-01-03 12:33:42 +0000

    Replaces fixed current weather icon with relevant icon from API

--- src/app.js
+++ src/app.js
@@ -20,12 +20,16 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let weatherCode = response.data.weather[0].icon;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherSummaryElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${weatherCode}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let city = "New York";
